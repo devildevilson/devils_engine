bfbe7ca447180bb8d61c0b46dfc11f06f5bbb0f1
Devil Devilson <devildevilson000@gmail.com>
2026-01-05 03:27:57 +0500

    remade global project include + remade atomic_pool - it must be used as thread pool from now on

--- libs/thread/src/thread/atomic_pool.cpp
+++ libs/thread/src/thread/atomic_pool.cpp
@@ -1,25 +1,54 @@
 #include "atomic_pool.h"
 
+constexpr auto ATOMIC_ONLY = std::memory_order_relaxed;
+constexpr auto PUBLISH = std::memory_order_release;
+constexpr auto CONSUME = std::memory_order_acquire;
+constexpr auto PUB_CONSUME = std::memory_order_acq_rel;
+
+// MSVC alignof(std::max_align_t) == alignof(double), WTF dude?
+constexpr size_t standart_aligment = 16;
+
 namespace devils_engine {
 namespace thread {
 
-atomic_pool::atomic_pool(const size_t thread_count) : busy_count(0) {
+atomic_pool::atomic_pool(const size_t thread_count) : stack_pool(MAXIMUM_TASK_SIZE * MAXIMUM_TASK_COUNT, MAXIMUM_TASK_SIZE, standart_aligment), busy_count(0) {
   for (size_t i = 0; i < thread_count; ++i) {
     workers.emplace_back([this] (std::stop_token stoken) {
-      while (!stoken.stop_requested()) {
-        task_t task;
+      size_t spins = 0;
 
-        while (!queue.try_pop(task) && !stoken.stop_requested()) {
-          std::this_thread::sleep_for(std::chrono::microseconds(1));
+      while (!stoken.stop_requested()) {
+        task_interface* task = nullptr;
+
+        if (queue.try_pop(task)) {
+          assert(task != nullptr);
+          spins = 0;
+          busy_count.fetch_add(1ull, PUB_CONSUME);
+          task->process();
+          stack_pool.destroy(task); // вернем память
+          busy_count.fetch_sub(1ull, PUB_CONSUME);
+          continue;
         }
 
-        // вот тут может быть ситуация когда последний поток взял в работу задачу
-        // но еще не пометил себя как работающий, насколько это для меня вообще пробелма?
-        // вообще ничего не мешает использовать какой нибудь глобальный счетчик задач
-        // который уменьшаться будет только с окончанием этого блока
-        busy_count.fetch_add(1ull, std::memory_order::relaxed);
-        if (task) task();
-        busy_count.fetch_add(-1ll, std::memory_order::relaxed);
+        if (spins < WORKER_SPIN_COUNT) {
+          spins += 1;
+          std::this_thread::yield();
+          continue;
+        }
+
+        {
+          std::unique_lock lk(cv_mtx);
+          cv.wait(lk, [&] { return queue.try_pop(task) || !stoken.stop_requested(); });
+        }
+
+        spins = 0;
+
+        if (task != nullptr) {
+          busy_count.fetch_add(1ull, PUB_CONSUME);
+          task->process();
+          stack_pool.destroy(task); // вернем память
+          busy_count.fetch_sub(1ull, PUB_CONSUME);
+          continue;
+        }
       }
     }, stop_source.get_token());
   }
@@ -27,34 +56,61 @@ atomic_pool::atomic_pool(const size_t thread_count) : busy_count(0) {
 
 atomic_pool::~atomic_pool() noexcept {
   stop_source.request_stop();
+  cv.notify_all();
 }
 
-void atomic_pool::submitbase(task_t f) noexcept {
+//void atomic_pool::submitbase(task_t f) noexcept {
+//  if (stop_source.stop_requested()) return;
+//  queue.push(std::move(f));
+//}
+
+void atomic_pool::submitbase(task_interface* t) noexcept {
   if (stop_source.stop_requested()) return;
-  queue.push(std::move(f));
+  queue.push(t);
+  cv.notify_one();
 }
 
 void atomic_pool::compute() {
-  while (!queue.was_empty()) {
-    task_t task;
+  size_t spins = 0;
 
-    while (!queue.try_pop(task) && !queue.was_empty()) {
-      std::this_thread::sleep_for(std::chrono::microseconds(1));
+  while (!queue.was_empty()) {
+    task_interface* task = nullptr;
+
+    if (queue.try_pop(task)) {
+      assert(task != nullptr);
+      spins = 0;
+      task->process();
+      stack_pool.destroy(task);
+      continue;
     }
 
-    if (task) task();
+    if (spins < WORKER_SPIN_COUNT) {
+      spins += 1;
+      std::this_thread::yield();
+      continue;
+    }
   }
 }
 
 void atomic_pool::compute(const size_t count) {
-  for (size_t i = 0; i < count && !queue.was_empty(); ++i) {
-    task_t task;
+  size_t spins = 0;
 
-    while (!queue.try_pop(task) && !queue.was_empty()) {
-      std::this_thread::sleep_for(std::chrono::microseconds(1));
+  for (size_t i = 0; i < count && !queue.was_empty(); ++i) {
+    task_interface* task = nullptr;
+
+    if (queue.try_pop(task)) {
+      assert(task != nullptr);
+      spins = 0;
+      task->process();
+      stack_pool.destroy(task);
+      continue;
     }
 
-    if (task) task();
+    if (spins < WORKER_SPIN_COUNT) {
+      spins += 1;
+      std::this_thread::yield();
+      continue;
+    }
   }
 }
 
@@ -79,7 +135,7 @@ uint32_t atomic_pool::thread_index(const std::thread::id id) const noexcept {
 
 size_t atomic_pool::size() const noexcept { return workers.size(); }
 size_t atomic_pool::tasks_count() const noexcept { return queue.was_size(); }
-size_t atomic_pool::working_count() const noexcept { return busy_count.load(std::memory_order::relaxed); }
+size_t atomic_pool::working_count() const noexcept { return busy_count.load(CONSUME); }
 size_t atomic_pool::queue_capacity() const noexcept { return queue.capacity(); }
 
 }
